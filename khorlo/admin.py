22a29b770a05ca727f57f67ad6433614a1c44150
KO <k.olszewski@o2.pl>
2015-03-21 19:02:12 +0100

    update

--- khorlo/admin.py
+++ khorlo/admin.py
@@ -1,16 +1,16 @@
 from copy import deepcopy
 from django.contrib import admin
 from mezzanine.core.admin import DisplayableAdmin, TabularDynamicInlineAdmin
-from .models import Author, Book
+#from .models import Author, Book
 
 author_extra_fieldsets = ((None, {"fields": ("dob",)}),)
 
-class BookInline(TabularDynamicInlineAdmin):
-    model = Book
-
-class AuthorAdmin(DisplayableAdmin):
-    inlines = (BookInline,)
-    fieldsets = deepcopy(DisplayableAdmin.fieldsets) + author_extra_fieldsets
-
-admin.site.register(Author, AuthorAdmin)
+# class BookInline(TabularDynamicInlineAdmin):
+#     model = Book
+#
+# class AuthorAdmin(DisplayableAdmin):
+#     inlines = (BookInline,)
+#     fieldsets = deepcopy(DisplayableAdmin.fieldsets) + author_extra_fieldsets
+#
+# admin.site.register(Author, AuthorAdmin)
 # Register your models here.
--- khorlo/models.py
+++ khorlo/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from mezzanine.core.models import Displayable, Ownable
 
 
-class Author(Displayable, Ownable):
-    dob = models.DateField("Date of birth")
-
-
-class Book(models.Model):
-    author = models.ForeignKey("Author")
-    cover = models.ImageField(upload_to="authors")
+# class Author(Displayable, Ownable):
+#     dob = models.DateField("Date of birth")
+#
+#
+# class Book(models.Model):
+#     author = models.ForeignKey("Author")
+#     cover = models.ImageField(upload_to="authors")
 
 
 
